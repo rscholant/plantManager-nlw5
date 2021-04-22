4952e3d17b1a03c45ca29a19dfa172294572c4df
Rafael Scholant <rafael.scholant@hotmail.com>
2021-04-21 23:27:12 -0300

    styles altered location

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ import {
   StyleSheet,
   TouchableOpacityProps,
 } from "react-native";
-import colors from "../../styles/colors";
+import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
 interface ButtonProps extends TouchableOpacityProps {
---
+++ src/pages/PlantSelect.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function PlantSelect() {
+  return (
+    <View>
+      <Text>Selecionar Planta</Text>
+    </View>
+  );
+}
+
+export default PlantSelect;
