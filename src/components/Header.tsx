826221d0a3ea954a6f64bdf416f309479e48d9f0
Rafael Scholant <rafael.scholant@hotmail.com>
2021-04-22 13:38:24 -0300

    Leason 3

---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, StyleSheet, Text, Image } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import userImg from "../assets/rafael.jpg";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+// import { Container } from './styles';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Rafael</Text>
+      </View>
+      <Image source={userImg} style={styles.image} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+  },
+  image: {
+    height: 70,
+    width: 70,
+    borderRadius: 40,
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  },
+});
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,11 +1,136 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import colors from "../styles/colors";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 
+import { EnviromentButton } from "../components/EnviromentButton";
+import { Header } from "../components/Header";
+import { Load } from "../components/Load";
+import PlantCardPrimary from "../components/PlantCardPrimary";
+import api from "../services/api";
+
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+interface EnviromentProps {
+  key: string;
+  title: string;
+}
+interface PlantProps {
+  id: string;
+  name: string;
+  about: string;
+  water_tips: string;
+  photo: string;
+  environments: [string];
+  frequency: {
+    times: number;
+    repeat_every: string;
+  };
+}
 function PlantSelect() {
+  const [enviroments, setEnviroments] = useState<EnviromentProps[]>([]);
+  const [plants, setPlants] = useState<PlantProps[]>([]);
+  const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
+  const [enviromentSelected, setEnviromentSelected] = useState("all");
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [loadedAll, setLoadedAll] = useState(false);
+
+  async function fetchPlants() {
+    const { data } = await api.get(
+      `plants?_sort=name&_order=asc&_page=${page}&_limit=8`
+    );
+    if (!data) return setLoading(true);
+    if (page > 1) {
+      setPlants((oldValue) => [...oldValue, ...data]);
+      setFilteredPlants((oldValue) => [...oldValue, ...data]);
+    } else {
+      setPlants(data);
+      setFilteredPlants(data);
+    }
+
+    setLoading(false);
+    setLoadingMore(false);
+  }
+
+  function handleEnviromentSelected(environment: string) {
+    setEnviromentSelected(environment);
+    if (environment == "all") return setFilteredPlants(plants);
+
+    const filtered = plants.filter((plant) =>
+      plant.environments.includes(environment)
+    );
+    setFilteredPlants(filtered);
+  }
+  function handleFetchMore(distance: number) {
+    if (distance < 1) return;
+    setLoadingMore(true);
+    setPage((oldValue) => oldValue + 1);
+    fetchPlants();
+  }
+
+  useEffect(() => {
+    async function fetchEnviroment() {
+      const { data } = await api.get(
+        "plants_environments?_sort=title&_order=asc"
+      );
+      setEnviroments([{ key: "all", title: "Todos" }, ...data]);
+    }
+
+    fetchEnviroment();
+  }, []);
+
+  useEffect(() => {
+    fetchPlants();
+  }, []);
+
+  if (loading) return <Load />;
   return (
     <View style={styles.container}>
-      <Text>Selecionar Planta</Text>
+      <View style={styles.header}>
+        <Header />
+        <Text style={styles.title}>Em qual ambiente</Text>
+        <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
+      </View>
+      <View>
+        <FlatList
+          data={enviroments}
+          renderItem={({ item }) => (
+            <EnviromentButton
+              key={item.key}
+              title={item.title}
+              active={item.key === enviromentSelected}
+              onPress={() => handleEnviromentSelected(item.key)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.enviromentList}
+        />
+      </View>
+      <View style={styles.plants}>
+        <FlatList
+          data={filteredPlants}
+          renderItem={({ item }) => (
+            <PlantCardPrimary key={item.id} data={item} />
+          )}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          onEndReachedThreshold={0.2}
+          onEndReached={({ distanceFromEnd }) =>
+            handleFetchMore(distanceFromEnd)
+          }
+          ListFooterComponent={
+            loadingMore ? <ActivityIndicator color={colors.green} /> : <></>
+          }
+        />
+      </View>
     </View>
   );
 }
@@ -13,9 +138,35 @@ function PlantSelect() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: colors.background,
+  },
+  header: {
+    paddingHorizontal: 30,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
+  },
+  enviromentList: {
+    height: 40,
+    justifyContent: "center",
+    paddingBottom: 5,
+    paddingLeft: 32,
+    marginVertical: 32,
+  },
+  plants: {
+    flex: 1,
+    paddingHorizontal: 32,
+    justifyContent: "center",
   },
 });
 
